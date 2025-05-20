8340c3bd33849451aff88fa732fc0556d7873371
AakashMasud <mubtasimmasud@gmail.com>
2025-05-21 09:28:13 +1000

    started parsing the altium csv BOMs with no filtering

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,21 @@
 //   / /| |/ __ `/ //_/ __ `/ ___/ __ \   / /|_/ / __ `/ ___/ / / / __  /
 //  / ___ / /_/ / ,< / /_/ (__  ) / / /  / /  / / /_/ (__  ) /_/ / /_/ /
 // /_/  |_\__,_/_/|_|\__,_/____/_/ /_/  /_/  /_/\__,_/____/\__,_/\__,_/
+use anyhow::Result;
 use std::io;
+
+use parse::read_preprocessed_bom;
 
 pub mod components;
 pub mod parse;
-fn main() {}
+
+fn main() -> Result<()> {
+    let data =
+        read_preprocessed_bom("/home/aakash_masud/Projects/bom_forge/test_BOMs/BOM_CAN_Ver.csv")?;
+
+    for entry in data {
+        println!("{:?}", entry);
+    }
+
+    Ok(())
+}
--- src/parse.rs
+++ src/parse.rs
@@ -1,15 +1,54 @@
-use anyhow;
+use anyhow::Result;
 use csv::ReaderBuilder;
+use std::collections::HashMap;
 use thiserror;
 
 #[derive(Debug)]
-struct PreprocessedBOMEntry {
+pub struct PreprocessedBOMEntry {
     name: String,
     description: String,
     mpn: String,
     designators: Vec<String>,
 }
 
-fn read_preprocessed_BOM(path: &str) -> Vec<PreprocessedBOMEntry> {
-    todo!()
+pub fn read_preprocessed_bom(path: &str) -> Result<Vec<PreprocessedBOMEntry>> {
+    let mut rdr = ReaderBuilder::new().flexible(true).from_path(path)?;
+    let headers = rdr.headers()?.clone();
+
+    let header_map: HashMap<_, _> = headers
+        .iter()
+        .enumerate()
+        .map(|(i, h)| (h.trim(), i))
+        .collect();
+
+    let mut entries = Vec::new();
+    for result in rdr.records() {
+        let record = result?;
+        let entry = PreprocessedBOMEntry {
+            name: extract_field("Name", &header_map, &record),
+            description: extract_field("Description", &header_map, &record),
+            mpn: extract_field("Part Number", &header_map, &record),
+            designators: extract_field("Designator", &header_map, &record)
+                .split(',')
+                .map(|s| s.to_string())
+                .collect(),
+        };
+        entries.push(entry);
+    }
+    Ok(entries)
+}
+
+//helper function for above
+// returns empty string if nothing found
+fn extract_field(
+    field_name: &str,
+    headers: &HashMap<&str, usize>,
+    record: &csv::StringRecord,
+) -> String {
+    if let Some(&index) = headers.get(&field_name) {
+        if let Some(val) = record.get(index) {
+            return val.trim().to_string();
+        }
+    }
+    String::new()
 }
