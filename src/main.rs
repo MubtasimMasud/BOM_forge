08daadd088429ca6bd140d3ec348a9f118491a9c
AakashMasud <mubtasimmasud@gmail.com>
2025-05-23 18:56:37 +1000

    Refactored so functions needed are methods of PreprocessedBOM and CPL files respectively

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,23 @@
 // /_/  |_\__,_/_/|_|\__,_/____/_/ /_/  /_/  /_/\__,_/____/\__,_/\__,_/
 use anyhow::Result;
 
-use parse::{find_ambigious_names, fix_ambigious_rows, parse_cpl, read_preprocessed_bom};
+use parse::{CplData, PreprocessedBOM};
 
 pub mod components;
 pub mod parse;
 
 fn main() -> Result<()> {
-    let data =
-        read_preprocessed_bom("/home/aakash_masud/Projects/bom_forge/test_BOMs/BOM_CAN_Ver.csv")?;
+    let bom_path = "/home/aakash_masud/Projects/bom_forge/test_BOMs/BOM_CAN_Ver.csv".to_string();
     let cpl_path =
         "/home/aakash_masud/Downloads/CANbus_verification_2025/Pick Place for PCB1.csv".to_string();
+    let mut bom = PreprocessedBOM::from_csv(&bom_path)?;
+    let mut cpl = CplData::from_csv(&cpl_path)?;
 
-    let ambigious = find_ambigious_names(data.clone());
-    println!("Ambigious Names");
-    println!("====================================================");
-    for name in ambigious {
-        println!("{:?} ewwwwwww", name);
-    }
-    println!("====================================================");
-    let cpl_data = parse_cpl(&cpl_path)?;
+    bom.fix_ambigious_rows(&cpl)?;
 
-    let data = fix_ambigious_rows(cpl_data, data)?;
-    for entry in data {
+    for entry in bom.bom {
         println!("{:?}", entry);
     }
+
     Ok(())
 }
--- src/parse.rs
+++ src/parse.rs
@@ -11,165 +11,188 @@ pub struct PreprocessedBOMEntry {
     designators: Vec<String>,
 }
 
-pub fn read_preprocessed_bom(path: &str) -> Result<Vec<PreprocessedBOMEntry>> {
-    let mut rdr = ReaderBuilder::new().flexible(true).from_path(path)?;
-    let headers = rdr.headers()?.clone();
-
-    let header_map: HashMap<_, _> = headers
-        .iter()
-        .enumerate()
-        .map(|(i, h)| (h.trim(), i))
-        .collect();
-
-    let mut entries = Vec::new();
-    for result in rdr.records() {
-        let record = result?;
-        let entry = PreprocessedBOMEntry {
-            name: extract_field("Name", &header_map, &record),
-            description: extract_field("Description", &header_map, &record),
-            mpn: extract_field("Part Number", &header_map, &record),
-            designators: extract_field("Designator", &header_map, &record)
-                .split(',')
-                .map(|s| s.to_string())
-                .collect(),
-        };
-        entries.push(entry);
-    }
-    Ok(entries)
+#[derive(Debug, Clone)]
+pub struct PreprocessedBOM {
+    pub bom: Vec<PreprocessedBOMEntry>,
 }
 
-//helper function for above
-// returns empty string if nothing found
-fn extract_field(
-    field_name: &str,
-    headers: &HashMap<&str, usize>,
-    record: &csv::StringRecord,
-) -> String {
-    if let Some(&index) = headers.get(&field_name) {
-        if let Some(val) = record.get(index) {
-            return val.trim().to_string();
-        }
-    }
-    String::new()
-}
+impl PreprocessedBOM {
+    pub fn from_csv(path: &str) -> Result<Self> {
+        let mut rdr = ReaderBuilder::new().flexible(true).from_path(path)?;
+        let headers = rdr.headers()?.clone();
 
-pub fn find_ambigious_names(rows: Vec<PreprocessedBOMEntry>) -> Vec<String> {
-    let mut ambigious_elements = Vec::new();
-
-    for row in rows {
-        if row.name.clone().split(',').count() > 1 {
-            ambigious_elements.push(row.name);
-        }
-    }
-    ambigious_elements
-}
-// Seeing what values are designated to which designators to address ambigious Name Coulms
-// returns Hashmap<Value, Designator> from the CPL file
-pub fn parse_cpl(path: &str) -> Result<Vec<(String, String)>> {
-    let mut rdr = ReaderBuilder::new().has_headers(false).from_path(path)?;
-
-    //Finding the header within the CPL file cuz it has an annoying title block
-    let mut header: Option<StringRecord> = None;
-    for result in rdr.records() {
-        let record = result?;
-        if record.get(0).map(|s| s.trim()) == Some("Designator") {
-            header = Some(record);
-            break;
-        }
-    }
-    let header = header.ok_or_else(|| anyhow!("bruh where is the header in this csv file"))?;
-
-    let header_map: HashMap<&str, usize> = header
-        .iter()
-        .enumerate()
-        .map(|(i, h)| (h.trim(), i))
-        .collect();
-
-    // adjust if your “comment” column is named differently
-    let designator_index = *header_map
-        .get("Designator")
-        .ok_or_else(|| anyhow!("invalid index"))?;
-    let comment_index = *header_map
-        .get("Comment")
-        .ok_or_else(|| anyhow!("invalid index"))?;
-
-    let mut parts_map: Vec<(String, String)> = Vec::new();
-    for result in rdr.records() {
-        let record = result?;
-        let designator = record
-            .get(designator_index)
-            .map(str::trim)
-            .ok_or_else(|| anyhow!("could not get designator"))?
-            .to_string();
-        let comment = record
-            .get(comment_index)
-            .map(str::trim)
-            .ok_or_else(|| anyhow!("could not get comment"))?
-            .to_string();
-
-        parts_map.push((comment, designator));
-    }
-    Ok(parts_map)
-}
-
-pub fn fix_ambigious_rows(
-    cpl_parts: Vec<(String, String)>,
-    bom_entries: Vec<PreprocessedBOMEntry>,
-) -> Result<Vec<PreprocessedBOMEntry>> {
-    let ambigous_names = find_ambigious_names(bom_entries.clone());
-    let fixed_entries = get_replacements_for_ambigious(cpl_parts.clone(), bom_entries.clone())?;
-
-    let mut filtered_bom: Vec<PreprocessedBOMEntry> = bom_entries
-        .clone()
-        .into_iter()
-        .filter(|entry| {
-            for name in ambigous_names.iter() {
-                if &entry.name == name {
-                    return false;
-                }
-            }
-            true
-        })
-        .collect();
-    for entry in fixed_entries {
-        filtered_bom.push(entry);
-    }
-    Ok(filtered_bom)
-}
-
-fn get_replacements_for_ambigious(
-    cpl_parts: Vec<(String, String)>,
-    bom_entries: Vec<PreprocessedBOMEntry>,
-) -> Result<Vec<PreprocessedBOMEntry>> {
-    let cases = find_ambigious_names(bom_entries);
-
-    let mut fixed_rows: Vec<PreprocessedBOMEntry> = Vec::new();
-    for case in cases {
-        // find designators for each value(name)
-        let names: Vec<String> = case
-            .clone()
-            .split(',')
-            .map(|s| s.trim().to_string())
+        let header_map: HashMap<_, _> = headers
+            .iter()
+            .enumerate()
+            .map(|(i, h)| (h.trim(), i))
             .collect();
-        for name in names {
+
+        let mut entries = Vec::new();
+        for result in rdr.records() {
+            let record = result?;
             let entry = PreprocessedBOMEntry {
-                name: name.clone(),
-                description: String::new(),
-                mpn: String::new(),
-                designators: find_designators_for_name(&name, &cpl_parts),
+                name: Self::extract_field("Name", &header_map, &record),
+                description: Self::extract_field("Description", &header_map, &record),
+                mpn: Self::extract_field("Part Number", &header_map, &record),
+                designators: Self::extract_field("Designator", &header_map, &record)
+                    .split(',')
+                    .map(|s| s.to_string())
+                    .collect(),
             };
-            fixed_rows.push(entry);
+            entries.push(entry);
         }
+        Ok(PreprocessedBOM { bom: entries })
     }
-    Ok(fixed_rows)
+    //helper function for above
+    // returns empty string if nothing found
+    fn extract_field(
+        field_name: &str,
+        headers: &HashMap<&str, usize>,
+        record: &csv::StringRecord,
+    ) -> String {
+        if let Some(&index) = headers.get(&field_name) {
+            if let Some(val) = record.get(index) {
+                return val.trim().to_string();
+            }
+        }
+        String::new()
+    }
+
+    pub fn fix_ambigious_rows(&mut self, cpl_parts: &CplData) -> Result<()> {
+        let ambigous_names = Self::find_ambigious_names(&self);
+        let fixed_entries = self.get_replacements_for_ambigious(&cpl_parts)?;
+
+        self.bom
+            .clone()
+            .into_iter()
+            .filter(|entry| {
+                for name in ambigous_names.iter() {
+                    if &entry.name == name {
+                        return false;
+                    }
+                }
+                true
+            })
+            .collect::<Vec<PreprocessedBOMEntry>>();
+        for entry in fixed_entries {
+            self.bom.push(entry);
+        }
+        Ok(())
+    }
+
+    fn get_replacements_for_ambigious(
+        &self,
+        cpl_parts: &CplData,
+    ) -> Result<Vec<PreprocessedBOMEntry>> {
+        let cases = Self::find_ambigious_names(self);
+
+        let mut fixed_rows: Vec<PreprocessedBOMEntry> = Vec::new();
+        for case in cases {
+            // find designators for each value(name)
+            let names: Vec<String> = case
+                .clone()
+                .split(',')
+                .map(|s| s.trim().to_string())
+                .collect();
+            for name in names {
+                let entry = PreprocessedBOMEntry {
+                    name: name.clone(),
+                    description: String::new(),
+                    mpn: String::new(),
+                    designators: Self::find_designators_for_name(&name, cpl_parts),
+                };
+                fixed_rows.push(entry);
+            }
+        }
+        Ok(fixed_rows)
+    }
+
+    pub fn find_designators_for_name(part_name: &str, cpl_parts: &CplData) -> Vec<String> {
+        let designators: Vec<String> = cpl_parts
+            .parts_map
+            .clone()
+            .into_iter()
+            .filter(|part| part.value == part_name)
+            .map(|part| part.designator)
+            .collect();
+        designators
+    }
+
+    pub fn find_ambigious_names(&self) -> Vec<String> {
+        let mut ambigious_elements = Vec::new();
+
+        for row in &self.bom {
+            if row.name.clone().split(',').count() > 1 {
+                ambigious_elements.push(row.name.clone());
+            }
+        }
+        ambigious_elements
+    }
 }
 
-fn find_designators_for_name(part_name: &str, cpl_parts: &Vec<(String, String)>) -> Vec<String> {
-    let designators: Vec<String> = cpl_parts
-        .clone()
-        .into_iter()
-        .filter(|(name, _)| name == part_name)
-        .map(|(_, designator)| designator)
-        .collect();
-    designators
+#[derive(Debug, Clone)]
+pub struct CplDataEntry {
+    designator: String,
+    value: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct CplData {
+    parts_map: Vec<CplDataEntry>,
+}
+
+impl CplData {
+    // Seeing what values are designated to which designators to address ambigious Name Coulms
+    // returns Hashmap<Value, Designator> from the CPL file
+    pub fn from_csv(path: &str) -> Result<Self> {
+        // Seeing what values are designated to which designators to address ambigious Name Coulms
+        // returns Hashmap<Value, Designator> from the CPL file
+        let mut rdr = ReaderBuilder::new().has_headers(false).from_path(path)?;
+
+        //Finding the header within the CPL file cuz it has an annoying title block
+        let mut header: Option<StringRecord> = None;
+        for result in rdr.records() {
+            let record = result?;
+            if record.get(0).map(|s| s.trim()) == Some("Designator") {
+                header = Some(record);
+                break;
+            }
+        }
+        let header = header.ok_or_else(|| anyhow!("bruh where is the header in this csv file"))?;
+
+        let header_map: HashMap<&str, usize> = header
+            .iter()
+            .enumerate()
+            .map(|(i, h)| (h.trim(), i))
+            .collect();
+
+        // adjust if your “comment” column is named differently
+        let designator_index = *header_map
+            .get("Designator")
+            .ok_or_else(|| anyhow!("invalid index"))?;
+        let comment_index = *header_map
+            .get("Comment")
+            .ok_or_else(|| anyhow!("invalid index"))?;
+
+        let mut parts_map: Vec<CplDataEntry> = Vec::new();
+        for result in rdr.records() {
+            let record = result?;
+            let designator = record
+                .get(designator_index)
+                .map(str::trim)
+                .ok_or_else(|| anyhow!("could not get designator"))?
+                .to_string();
+            let comment = record
+                .get(comment_index)
+                .map(str::trim)
+                .ok_or_else(|| anyhow!("could not get comment"))?
+                .to_string();
+
+            parts_map.push(CplDataEntry {
+                designator: designator,
+                value: comment,
+            });
+        }
+        Ok(CplData { parts_map })
+    }
 }
